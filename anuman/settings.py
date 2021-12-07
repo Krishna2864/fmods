26ac1ac0178db0f6c5b9fc8573b5465cbc02bc33
KrishnaDhakad <local@locals-MacBook-Pro.local>
2021-12-07 14:09:37 +0530

    com

--- anuman/settings.py
+++ anuman/settings.py
@@ -115,11 +115,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = os.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
